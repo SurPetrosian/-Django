753339d2db23696069e5e968395c6ccf78826c43
SurPetrosian <surach2020@gmail.com>
2025-05-14 17:19:03 +0300

    создание контроллеров и шаблонов

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+from .models import Blog
+from .forms import BlogForm
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'blogs'
+
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True)
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'blog'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.number_of_views += 1
+        obj.save()
+
+        if obj.number_of_views == 100:
+            send_mail(
+                subject='🎉 Поздравляем!',
+                message=f'Статья "{obj.header}" достигла 100 просмотров!',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[settings.DEFAULT_FROM_EMAIL],
+                fail_silently=True,
+            )
+        return obj
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog:list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog/blog_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('blog:detail', kwargs={'pk': self.object.pk})
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog:list')
