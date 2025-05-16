b71aade27a0174be4e27d94f057ae1cfba1aefd8
SurPetrosian <surach2020@gmail.com>
2025-05-17 00:57:19 +0300

    добавление админки

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('header', 'is_published', 'created_at')
+    list_filter = ('is_published',)
+    search_fields = ('header', 'content')
