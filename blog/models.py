6c9b0ffd6b55a21ec072d6718a2cb50823478732
SurPetrosian <surach2020@gmail.com>
2025-05-17 00:58:17 +0300

    фиксы в моделе

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ class Blog(models.Model):
     header = models.CharField(max_length=300, verbose_name='заголовок')
     content = models.TextField(verbose_name='содержимое', null=True, blank=True)
     image = models.ImageField( verbose_name='изображение',null=True, blank=True)
-    created_at = models.DateField(verbose_name='дата создания')
+    created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
     number_of_views = models.BigIntegerField(verbose_name='количество просмотров',default=0)
 
